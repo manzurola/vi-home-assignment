3379b618f388c0ef902cd9c9c92c07e0795182ad
Guy Manzurola <guy.manzurola@gmail.com>
2025-07-23 21:56:13 +0300

    Cleanups (#4)
    * lint service
    
    * optimize imports

--- backend/marvel-service/src/movie-explorer/movie-explorer.module.ts
+++ backend/marvel-service/src/movie-explorer/movie-explorer.module.ts
@@ -1,8 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataScraperModule } from '../data-scraper/data-scraper.module';
-import databaseConfig from '../config/database.config';
 import { MovieExplorerController } from './movie-explorer.controller';
 import { MovieExplorerService } from './movie-explorer.service';
 import { MovieExplorerRepository } from './movie-explorer.repository';
